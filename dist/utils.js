2665677c64ca2f0689fb0b2c1c8564a7da826d2c
n0wlk <kxwxn@icloud.com>
2024-03-13 00:35:18 +0900

    webpack updated

---
+++ dist/utils.js
@@ -0,0 +1,10 @@
+import _ from "lodash";
+export const add = (x, y) => {
+    return _.add(x, y);
+};
+export const multiply = (x, y) => {
+    return x * y;
+};
+export const divide = (x, y) => {
+    return x / y;
+};
---
+++ src/Dog.ts
@@ -0,0 +1,10 @@
+export default class Dog {
+  constructor(
+    public name: string,
+    public breed: string,
+    public age: number,
+  ) {}
+  bark() {
+    console.log("WOOF WOOF");
+  }
+}
